d95ddb212a4790d865a3df106ba88400de6f0ee6
Patryk <patryk.leczycki1@gmail.com>
2019-04-23 20:10:36 +0200

    weekday entity

---
+++ src/main/java/pl/patlec/model/Weekday.java
@@ -0,0 +1,23 @@
+package pl.patlec.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+public class Weekday {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int sequence;
+}
