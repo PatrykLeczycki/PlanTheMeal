471f91526bb4ebc3f35799d6b14db7ec42d0000d
Patryk <patryk.leczycki1@gmail.com>
2019-04-23 20:09:15 +0200

    recipe entity

---
+++ src/main/java/pl/patlec/model/Recipe.java
@@ -0,0 +1,41 @@
+package pl.patlec.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String ingredients; // TODO: create ingredient entity
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private LocalDateTime created;
+
+    private LocalDateTime updated;
+
+    @NotNull
+    private int preparationTime;
+
+    @NotNull
+    private String preparation;
+
+    @ManyToOne
+    @NotNull
+    private User author;
+}
