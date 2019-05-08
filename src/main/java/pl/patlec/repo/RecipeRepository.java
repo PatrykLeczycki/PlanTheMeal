d4c09b8c18ff52564e3e142abe0a4d234668894d
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-05-08 16:56:20 +0200

    added counting author's recipes

--- src/main/java/pl/patlec/repo/RecipeRepository.java
+++ src/main/java/pl/patlec/repo/RecipeRepository.java
@@ -3,8 +3,10 @@ package pl.patlec.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.patlec.model.Recipe;
+import pl.patlec.model.User;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
+    Long countAllByAuthor(User user);
 
 }
