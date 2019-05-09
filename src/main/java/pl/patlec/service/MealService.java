e6909e4219fe809e04f56e5a70b61d3bbdb1456f
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-05-10 01:14:11 +0200

    added counting methods

--- src/main/java/pl/patlec/service/MealService.java
+++ src/main/java/pl/patlec/service/MealService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.patlec.model.Meal;
 import pl.patlec.model.Plan;
+import pl.patlec.model.Recipe;
 import pl.patlec.repo.MealRepository;
 
 import java.util.List;
@@ -24,5 +25,12 @@ public class MealService {
 
     public List<Meal> getAllByPlan(Plan plan){
         return mealRepository.findAllByPlanOrderByWeekdayIdAscSequenceAsc(plan);
+    }
+
+    public Long countAllByPlan(Plan plan){
+        return mealRepository.countAllByPlan(plan);
+    }
+    public Long countAllByRecipe(Recipe recipe){
+        return mealRepository.countAllByRecipe(recipe);
     }
 }
