c9a296004520c9bc9bbec36bf40444fe0605b8b0
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-05-10 01:13:58 +0200

    added jpa methods

--- src/main/java/pl/patlec/repo/MealRepository.java
+++ src/main/java/pl/patlec/repo/MealRepository.java
@@ -3,12 +3,18 @@ package pl.patlec.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.patlec.model.Meal;
 import pl.patlec.model.Plan;
+import pl.patlec.model.Recipe;
 
 import java.util.List;
 
 public interface MealRepository extends JpaRepository<Meal, Long> {
 
     List<Meal> findAllByPlanOrderByWeekdayIdAscSequenceAsc(Plan plan);
+
+    Long countAllByPlan(Plan plan);
+    Long countAllByRecipe(Recipe recipe);
+
+
 
 
 }
