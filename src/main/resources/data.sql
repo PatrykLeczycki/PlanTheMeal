7d1b2872d3f1042cd01bc128816e296c1d3e5a93
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-05-13 22:31:18 +0200

    added user for tests

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,15 @@
-insert into role (name)
-values ('ROLE_ADMIN');
+insert into role (name) values ('ROLE_ADMIN');
 
-insert into role (name)
-values ('ROLE_USER');
+insert into role (name) values ('ROLE_USER');
 
 insert into user(id, email, password, name, surname, enabled, registration_token, pass_recovery_token) values (null, "patryk.leczycki1@gmail.com","$2a$10$U68vFDnV45kPvK4KO42RAeQY6UM7/N0UpwpWUEPZpTutRWQcHhYZ.", "Patryk", "Łęczycki", 1, null, null);
+insert into user(id, email, password, name, surname, enabled, registration_token, pass_recovery_token) values (null, "smryku@gmail.com","$2a$10$nN5fg/Pa2cAsLBhO/4.Do.aN6cd9yqDKzwCUynL5MrR1AXIUewh3y", "Patryk", "Łęczycki", 1, null, null);
 
-insert into user_role (user_id, role_id)
-values (1, 1);
+insert into user_role (user_id, role_id) values (1, 1);
 
-insert into user_role (USER_ID, ROLE_ID)
-values (1, 2);
+insert into user_role (USER_ID, ROLE_ID) values (1, 2);
+
+insert into user_role (USER_ID, ROLE_ID) values (2, 2);
 
 insert into weekday values(null, "Monday", 1);
 insert into weekday values(null, "Tuesday", 2);
