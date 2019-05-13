d93473244d17d1998d28370afc514b5b93060ff6
Patryk Łęczycki <patryk.leczycki1@gmail.com>
2019-05-13 22:34:29 +0200

    modified plan service methods

--- src/main/java/pl/patlec/service/PlanService.java
+++ src/main/java/pl/patlec/service/PlanService.java
@@ -10,6 +10,7 @@ import pl.patlec.repo.PlanRepository;
 import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -29,7 +30,12 @@ public class PlanService {
     }
 
     public Long getUserLastPlanId(Principal principal){
-        return planRepository.findTopByAuthorOrderByCreatedDesc(userService.findUserByEmail(principal.getName())).getId();
+
+        Plan lastPlan = planRepository.findTopByAuthorOrderByCreatedDesc(userService.findUserByEmail(principal.getName()));
+
+        if(!Objects.isNull(lastPlan))
+            return lastPlan.getId();
+        return null;
     }
 
     public Long countByUser(User user){
@@ -48,12 +54,38 @@ public class PlanService {
         planRepository.save(plan);
     }
 
-    public void edit(Plan plan){
+    public boolean edit(Plan plan, String name, String description, Principal principal){
+
+
+        User loggedUser = userService.findUserByEmail(principal.getName());
+
+        if((!loggedUser.equals(plan.getAuthor()) && !userService.isAdmin(loggedUser))){
+            prompt.getNames().add("accessdenied");
+        } else if(name.length() < 1 || description.length() < 1 || (!loggedUser.equals(plan.getAuthor()) && !userService.isAdmin(loggedUser))){
+            if(name.length() < 1)
+                prompt.getNames().add("emptyname");
+            if(description.length() < 1)
+                prompt.getNames().add("emptydesc");
+
+            return false;
+        }
+
+        plan.setName(name);
+        plan.setDescription(description);
+
         planRepository.save(plan);
+
+        return true;
     }
 
-    public void delete(Plan plan){
-        if(mealService.countAllByPlan(plan) > 0){
+    public void delete(Plan plan, Principal principal){
+
+        User loggedUser = userService.findUserByEmail(principal.getName());
+
+        if(!loggedUser.equals(plan.getAuthor()) && !userService.isAdmin(loggedUser)){
+            prompt.add("accessdenied");
+        }
+        else if(mealService.countAllByPlan(plan) > 0){
             prompt.add("mealinplan");
             prompt.getAdditionalInfo().put("mealinplanwithid", "" + plan.getId());
         }
